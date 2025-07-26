4e495994234b7871511f6d0f1454b01540443215
Puja Shaw <pujakm9163@gmail.com>
2025-07-26 13:42:33 +0530

    Update firstProgram.js

--- firstProgram.js
+++ firstProgram.js
@@ -1,5 +1,19 @@
-
-
+// Print number from 1 to 10
+console.log("1 to 10 numbers");
 for(let i=1; i<=10; i++){
+  console.log(i);
+}
+
+// Print Even number from 1 to 10
+console.log("1 to 10 Even numbers");
+for(let i=1; i<=10; i++){
+  if( i % 2 == 0)
+  console.log(i);
+}
+
+// Print Odd number from 1 to 10
+console.log("1 to 10 Odd numbers");
+for(let i=1; i<=10; i++){
+  if( i % 2 != 0)
   console.log(i);
 }
